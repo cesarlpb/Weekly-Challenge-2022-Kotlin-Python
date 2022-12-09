38f8e18fc7847c09c9b3e85eba314573f79b8a49
César <cesarlpb@gmail.com>
2022-12-09 11:01:40 +0100

    Reto 3 - completo

--- python/challenges/Challenge3.py
+++ python/challenges/Challenge3.py
@@ -15,30 +15,37 @@
 #  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
 #  *
 #  */
+#%% isPrime
+    # primo := solo es divisible por sí mismo y 1 -> Ej: 1, 2, 3, 5, 7, 11, 13 ...
+    # no primo := tiene divisores distintos de sí mismo y 1 -> Ej: 4, 6, 8, 10, 12 ...
+# Complejidad
+    # tiempo -> N * N / 2 -> O(N^2)
+    # espacio -> O(1)
 
-# fun main() {
+def isPrime(num):
+    for divisor in range(2,num // 2 + 1):
+        if num % divisor == 0:
+            return False
+    return True
 
-#     (1..100).forEach { number ->
-#         if (isPrime(number)) {
-#             println(number)
-#         }
-#     }
-# }
+def main():
+    N = 100
+    for i in range(1,N+1):
+        if isPrime(i):
+            print(i)
+main()
 
-# private fun isPrime(number: Int): Boolean {
-
-#     if (number < 2) {
-#         return false
-#     }
-
-#     for (i in 2 until number) {
-#         if (number % i == 0) {
-#             return false
-#         }
-#     }
-
-#     return true
-# }
-
-
-
+# Posibles mejoras
+    # Descartar los números pares porque salvo el 2, son divisibles por 2 => no primos
+    # Números que terminan en 5 o 0 -> descartados por ser divisible por 5 o 10 respectivamente
+    # Suma de los dígitos es múltiplo de 3 o 9 -> divisible por 3 o 9
+    # Etc.
+# Algunas reglas de divisibilidad que se podrían implementar:
+    # Un número entero es divisible por 2 si su último dígito es 0, 2, 4, 6 o 8.
+    # Un número entero es divisible por 3 si la suma de sus dígitos es divisible por 3.
+    # Un número entero es divisible por 4 si el número formado por sus dos últimos dígitos es divisible por 4.
+    # Un número entero es divisible por 5 si su último dígito es 0 o 5.
+    # Un número entero es divisible por 6 si es divisible por 2 y 3.
+    # Un número entero es divisible por 9 si la suma de sus dígitos es divisible por 9.
+    # Un número entero es divisible por 10 si su último dígito es 0.
+    # Un número entero es divisible por 11 si la diferencia entre la suma de los dígitos en las posiciones pares y la suma de los dígitos en las posiciones impares es divisible por 11.
