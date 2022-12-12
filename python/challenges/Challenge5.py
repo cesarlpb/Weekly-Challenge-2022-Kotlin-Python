2cb520c11c30195f493add42ddf4bde49896d13e
César <cesarlpb@gmail.com>
2022-12-12 16:34:55 +0100

    Reto 5 - bajamos imagen y accedemos a datos de ancho y alto en px

--- python/challenges/Challenge5.py
+++ python/challenges/Challenge5.py
@@ -19,9 +19,34 @@
 #  * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
 #  *
 #  */
+#%% 1. Bajar la imagen a partir de la URL -> request y la guardamos en local con shutils
+# https://www.scrapingbee.com/blog/download-image-python/
+import requests # request img from web
+import shutil # save img locally
 
-# 1. Bajar la imagen a partir de la URL -> request
-# 2. Obtener los datos de la imagen de ancho y alto -> PIL
+url = "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
+response = requests.get(url, stream = True)
+filename = "img/imagen1.png"
+if response.status_code == 200:
+    with open(filename,'wb') as f:
+        shutil.copyfileobj(response.raw, f)
+    print('Hemos guardado la imagen como:',filename)
+else:
+    print('La img no se pudo descargar :(')
+#%% 2. Obtener los datos de la imagen de ancho y alto -> PIL
+# import required module
+from PIL import Image
+  
+# nombre del archivo
+file_path = "img/imagen.png"
+img = Image.open(file_path)
+  
+# Ancho y alto de img
+width = img.width
+height = img.height
+
+print("Ancho:", width, "px")
+print("Altura:", height, "px")
 # 3. Calcular el aspect ratio como el cociente y simplificar a una "fracción simple" -> Fraction
 
 # class Challenge5() {
@@ -64,3 +89,4 @@
 #     }
 
 # }
+# %%
