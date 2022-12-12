2117ce29ee0f75749c9d0ad2c41d0d0ba8a783f2
César <cesarlpb@gmail.com>
2022-12-12 17:01:08 +0100

    Probamos con otra imagen

--- python/challenges/Challenge5.py
+++ python/challenges/Challenge5.py
@@ -24,7 +24,7 @@
 import requests # request img from web
 import shutil # save img locally
 
-url = "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
+url = "https://www.freecodecamp.org/news/content/images/size/w2000/2022/02/Banner-10.png"
 response = requests.get(url, stream = True)
 filename = "img/imagen1.png"
 if response.status_code == 200:
@@ -34,11 +34,12 @@ if response.status_code == 200:
 else:
     print('La img no se pudo descargar :(')
 #%% 2. Obtener los datos de la imagen de ancho y alto -> PIL
-# import required module
+# https://www.geeksforgeeks.org/how-to-find-width-and-height-of-an-image-using-python/
+# Importamos Image de PIL (pillow)
 from PIL import Image
   
 # nombre del archivo
-file_path = "img/imagen.png"
+file_path = "img/imagen1.png"
 img = Image.open(file_path)
   
 # Ancho y alto de img
@@ -47,46 +48,10 @@ height = img.height
 
 print("Ancho:", width, "px")
 print("Altura:", height, "px")
-# 3. Calcular el aspect ratio como el cociente y simplificar a una "fracción simple" -> Fraction
-
-# class Challenge5() {
-
-#     fun aspectRatio(url: String) {
-
-#         Thread {
-#             var aspectRationStr: String? = null
-
-#             val url = URL(url)
-#             val bitmap = BitmapFactory.decodeStream(url.openStream())
-
-#             val height = bitmap.height
-#             val width = bitmap.width
-#             val aspectRatio = rationalAspectRatio(height.toDouble() / width.toDouble())
-#             aspectRationStr = "${aspectRatio.second}:${aspectRatio.first}"
-
-#             aspectRationStr?.let { ratio ->
-#                 println("El aspect ratio es ${ratio}")
-#             } ?: run {
-#                 println("No se ha podido calcular el aspect ratio")
-#             }
-#         }.start()
-#     }
-
-#     data class Quadruple(val h1: Int, val k1: Int, val h: Int, val k: Int)
-
-#     private fun rationalAspectRatio(aspectRatio: Double): Pair<Int, Int> {
-#         val precision = 1.0E-6
-#         var x = aspectRatio
-#         var a = x.roundToInt()
-#         var q = Quadruple(1, 0, a, 1)
-
-#         while (x - a > precision * q.k.toDouble() * q.k.toDouble()) {
-#             x = 1.0 / (x - a)
-#             a = x.roundToInt()
-#             q = Quadruple(q.h, q.k, q.h1 + a * q.h, q.k1 + a * q.k)
-#         }
-#         return Pair(q.h, q.k)
-#     }
-
-# }
+#%% 3. Calcular el aspect ratio como el cociente y simplificar a una "fracción simple" -> Fraction
+# https://stackoverflow.com/questions/23344185/how-to-convert-a-decimal-number-into-fraction
+from fractions import Fraction
+aspect_ratio = round(width / height, 10)
+fraccion = Fraction(aspect_ratio).limit_denominator()
+print("Aspect ratio:", fraccion)
 # %%
