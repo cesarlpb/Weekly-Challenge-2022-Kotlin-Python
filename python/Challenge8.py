8d607c52fa6eabef7c2be9a75df2fdab6aef01ab
César <cesarlpb@gmail.com>
2022-12-13 10:54:44 +0100

    Reto 8 - pasar float a binario

--- python/Challenge8.py
+++ python/Challenge8.py
@@ -28,36 +28,29 @@
 # 125 => 1111101 = 1*2^6 + 1*2^5 + 1*2^4 + 1*2^3 + 1*2^2 + 0*2^1 + 1*2^0 = 125
 #%% main
 def main():
-    print("10 en binario:", decimal_a_binario(10))   # 1010
-    print("125 en binario:", decimal_a_binario(125)) # 1111101
-def decimal_a_binario(int):
-    cociente = int
+    print("10 en binario:", int_a_binario(10))   # 1010
+    print("125 en binario:", int_a_binario(125)) # 1111101
+
+    print("125.25 en binario:", float_a_binario(125.25)) # 1111101.01
+    print("125.5 en binario:", float_a_binario(125.5)) # 1111101.1
+    print("125.1 en binario:", float_a_binario(125.1)) # 1111101.??????
+    # print("125.75 en binario:", float_a_binario(125.75)) # 1111101.11
+def int_a_binario(entero):
+    cociente = int(entero)
     str_binario = ""
     while cociente > 0:
         str_binario += str(cociente % 2)
         cociente = cociente // 2
     return str_binario[::-1].encode('utf-8')
+def float_a_binario(num):
+    exp = 0
+    my_num = num
+    
+    while not my_num.is_integer() and exp < 10:
+        exp += 1
+        my_num = num * 2**exp
+
+    my_str = int_a_binario(my_num).decode('utf-8')
+    my_str = my_str[:-exp] + "." +my_str[-exp:]
+    return (my_str, exp)
 main()
-# fun main() {
-#     println(decimalToBinary(387))
-#     println(decimalToBinary(0))
-# }
-
-# fun decimalToBinary(decimal: Int): String {
-
-#     var number = decimal
-#     var binary = ""
-
-#     while (number != 0) {
-
-#         val reminder = number % 2
-#         number /= 2
-
-#         binary = "$reminder$binary"
-#     }
-
-#     return binary.ifEmpty { "0" }
-# }
-
-# # 
-# %%
