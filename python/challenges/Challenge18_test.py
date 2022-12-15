e4b38fa2de1415350f93fc44da88e6c4fe8c30bc
César <cesarlpb@gmail.com>
2022-12-15 14:39:42 +0100

    pytest - ejemplos - código inicial

--- python/challenges/Challenge18_test.py
+++ python/challenges/Challenge18_test.py
@@ -1,14 +1,1 @@
-# pytest
-# content of test_sample.py
-def my_fun(x):
-    return x + 1
-
-
-def test_my_fun():
-    assert my_fun(3) == 4
-
-def fun():
-    return True
-
-def test_fun():
-    assert fun() == True
+# Tests de Challenge18
